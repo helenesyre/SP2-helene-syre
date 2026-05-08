54b45bf07040522e56ea1b69118483df4a4a75a0
Helene Syre <syrehelene@gmail.com>
2026-05-08 14:29:01 +0200

    Update comingSoon.js

--- src/pages/comingSoon.js
+++ src/pages/comingSoon.js
@@ -1,17 +1,9 @@
 export function comingSoon() {
   return `
-    <div class="flex flex-col items-center gap-6 py-16">
+    <div class="flex flex-col items-center gap-6 py-16 px-6 md:px-8 lg:px-16 text-center">
       <h1 class="text-4xl font-bold">Coming Soon</h1>
       <p class="text-lg">This feature is currently under development. Stay tuned for updates!</p>
-      <div class="flex flex-wrap gap-4 justify-center">
-        <a href="#/" class="btn-small btn-border">Go to Home</a>
-        <a href="#/listing" class="btn-small btn-border">Go to Listing</a>
-        <a href="#/profile" class="btn-small btn-border">Go to Profile</a>
-        <a href="#/login" class="btn-small btn-border">Go to Login</a>
-        <a href="#/register" class="btn-small btn-border">Go to Register</a>
-        <a href="#/page-not-found" class="btn-small btn-border">Go to Page Not Found</a>
-      </div>
-    </div>
+      <a href="#/" class="btn-small btn-border">Go to Home</a>
     </div>
   `;
 }
