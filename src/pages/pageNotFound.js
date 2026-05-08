3b611da096b35ee642643ee3aaed16869754bb39
Helene Syre <syrehelene@gmail.com>
2026-05-08 14:27:33 +0200

    Update pageNotFound.js

--- src/pages/pageNotFound.js
+++ src/pages/pageNotFound.js
@@ -5,11 +5,8 @@ export function pageNotFound() {
     <div class="flex flex-col items-center gap-6 py-16 px-6 md:px-8 lg:px-16 text-center">
       <img src="${image404}" alt="404 Error - Page Not Found" class="w-full max-w-2xl">
       <h1 class="text-4xl font-bold">Oops! Page not found</h1>
-      <p class="text-lg">The page you are looking for does not exist. Please check the URL or return to the <a href="#/" class="text-blue-500 hover:underline">home page</a>.</p>
-      <div class="flex flex-wrap gap-4 justify-center">
-        <a href="#/" class="btn-small btn-border">Go to Home</a>
-        <a href="#/login" class="btn-small btn-border">Sign in</a>
-      </div>
+      <p class="text-lg">The page you are looking for does not exist. Please check the URL or return to the home page.</p>
+      <a href="#/" class="btn-small btn-border">Go to Home</a>
     </div>
   `;
 }
