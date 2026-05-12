d5085ad9b11a889196295e2027eacef5639b9863
Helene Syre <syrehelene@gmail.com>
2026-05-13 01:22:36 +0200

    Added bold text display to prefix in toast

--- src/assets/js/components/toasts/toast.js
+++ src/assets/js/components/toasts/toast.js
@@ -10,6 +10,7 @@ import { renderIcons } from '../../utils/icons.js';
 export function showToast(message, type = "success", wait = 5000) {
   const toast = document.createElement("div");
   const iconElement = document.createElement("i");
+  const prefix = document.createElement("span");
   const titleElement = document.createElement("h4");
   const textContentElement = document.createElement("div");
   const closeButton = document.createElement("button");
@@ -21,7 +22,8 @@ export function showToast(message, type = "success", wait = 5000) {
       textContentElement.appendChild(iconElement);
       textContentElement.classList.add("flex", "items-center", "gap-2");
       iconElement.setAttribute("data-lucide", "circle-check");
-      iconElement.classList.add("size-6", "shrink-0")
+      iconElement.classList.add("size-6", "shrink-0");
+      prefix.textContent = "Success: ";
       break;
     case "error":
       toast.classList.add(`bg-feedback-error-bg`, `text-feedback-error-text`);
@@ -29,6 +31,7 @@ export function showToast(message, type = "success", wait = 5000) {
       textContentElement.classList.add("flex", "items-center", "gap-2");
       iconElement.setAttribute("data-lucide", "alert-circle");
       iconElement.classList.add("size-6", "shrink-0", "text-feedback-error-icon");
+      prefix.textContent = "Error: ";
       break;
     default:
       toast.classList.add(`bg-feedback-info-bg`, `text-feedback-info-text`);
@@ -36,9 +39,19 @@ export function showToast(message, type = "success", wait = 5000) {
       textContentElement.classList.add("flex", "items-center", "gap-2");
       iconElement.setAttribute("data-lucide", "info");
       iconElement.classList.add("size-6", "shrink-0")
+      prefix.textContent = "";
   }
-
-  titleElement.textContent = message;
+  if (message.split(": ").length > 1) {
+    const status = message.split(": ")[0]; // Get the status code
+    prefix.textContent = `${status}: `;
+    prefix.classList.add("font-bold");
+    titleElement.appendChild(prefix);
+    message = message.split(": ")[1]; // Get the part after the status code for error messages
+  } else {
+    prefix.classList.add("font-bold");
+    titleElement.appendChild(prefix);
+  }
+  titleElement.appendChild(document.createTextNode(message));
   closeButton.innerHTML = `<i data-lucide="x" class="size-6 cursor-pointer"></i>`;
   closeButton.onclick = () => {
     toast.remove();
