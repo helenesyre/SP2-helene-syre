2ed96101adfd21d146e1036e2967c4c757952646
Helene Syre <syrehelene@gmail.com>
2026-05-15 19:15:47 +0200

    Updated bio

--- src/assets/js/utils/useAuth.js
+++ src/assets/js/utils/useAuth.js
@@ -53,7 +53,7 @@ export function useAuth() {
       name: username,
       email: email,
       password: password,
-      bio: "This is my NightNode bio",
+      bio: "This is my bio",
       avatar: {
         url: "https://images.unsplash.com/photo-1680355455579-e851904eb784?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         alt: "Placeholder avatar"
